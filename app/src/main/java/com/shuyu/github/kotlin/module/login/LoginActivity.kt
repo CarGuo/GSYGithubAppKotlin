341fcbcb636acf06084419be17460900d19a286f
guoshuyu <359369982@qq.com>
2018-09-28 16:45:47 +0800

    自定义ttf

---
+++ app/src/main/java/com/shuyu/github/kotlin/module/login/LoginActivity.kt
@@ -0,0 +1,14 @@
+package com.shuyu.github.kotlin.module.login
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by guoshuyu
+ * Date: 2018-09-28
+ */
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
