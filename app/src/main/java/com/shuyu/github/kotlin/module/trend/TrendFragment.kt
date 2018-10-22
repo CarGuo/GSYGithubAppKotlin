509bb1976ab9990ea0c346982e5d0f92787410e2
guoshuyu <359369982@qq.com>
2018-10-22 11:42:13 +0800

    add drop menu

--- app/src/main/java/com/shuyu/github/kotlin/module/trend/TrendFragment.kt
+++ app/src/main/java/com/shuyu/github/kotlin/module/trend/TrendFragment.kt
@@ -1,24 +1,20 @@
 package com.shuyu.github.kotlin.module.trend
 
-import android.arch.lifecycle.Observer
-import android.arch.lifecycle.ViewModelProvider
-import android.arch.lifecycle.ViewModelProviders
 import android.content.Context
-import android.databinding.ViewDataBinding
 import android.os.Bundle
 import android.support.v7.widget.RecyclerView
 import android.view.View
+import android.view.ViewGroup
+import android.widget.ListView
 import com.shuyu.commonrecycler.BindSuperAdapterManager
 import com.shuyu.github.kotlin.R
-import com.shuyu.github.kotlin.databinding.FragmentListBinding
+import com.shuyu.github.kotlin.databinding.FragmentTrendBinding
 import com.shuyu.github.kotlin.holder.ReposHolder
-import com.shuyu.github.kotlin.holder.base.BindingDataRecyclerManager
 import com.shuyu.github.kotlin.model.ui.ReposUIModel
 import com.shuyu.github.kotlin.module.base.BaseListFragment
-import com.shuyu.github.kotlin.module.base.BaseViewModel
-import com.shuyu.github.kotlin.module.base.autoCleared
-import kotlinx.android.synthetic.main.fragment_list.*
-import javax.inject.Inject
+import com.shuyu.github.kotlin.ui.adapter.ListDropDownAdapter
+import kotlinx.android.synthetic.main.fragment_trend.*
+
 
 /**
  * 趋势
@@ -26,14 +22,48 @@ import javax.inject.Inject
  * Date: 2018-09-28
  */
 
-class TrendFragment : BaseListFragment<FragmentListBinding, TrendViewModel>() {
+class TrendFragment : BaseListFragment<FragmentTrendBinding, TrendViewModel>() {
+
+    private val citys = listOf("不限", "武汉", "北京", "上海", "成都", "广州", "深圳", "重庆", "天津", "西安", "南京", "杭州")
+
+    private val ages = listOf("不限", "18岁以下", "18-22岁", "23-26岁", "27-35岁", "35岁以上")
+
+    private lateinit var baseRecycler: RecyclerView
 
     override fun getLayoutId(): Int {
-        return R.layout.fragment_list
+        return R.layout.fragment_trend
+    }
+
+    override fun onCreateView(mainView: View?) {
+        super.onCreateView(mainView)
+        //init context view
+        baseRecycler = RecyclerView(activity)
+        baseRecycler.layoutParams = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+
+        val sexView = ListView(activity)
+        sexView.dividerHeight = 0
+        val sexAdapter = ListDropDownAdapter(context!!, ages)
+        sexView.adapter = sexAdapter
+
+        val sexView2 = ListView(activity)
+        sexView2.dividerHeight = 0
+        val sexAdapter2 = ListDropDownAdapter(context!!, citys)
+        sexView2.adapter = sexAdapter2
+
+
+        val popupViews = ArrayList<View>()
+        popupViews.add(sexView)
+        popupViews.add(sexView2)
+
+        val headers = listOf("城市", "城市2")
+
+        trend_drop_menu.setDropDownMenu(headers, popupViews, baseRecycler)
+
     }
 
     override fun onItemClick(context: Context, position: Int) {
---
+++ app/src/main/java/com/shuyu/github/kotlin/ui/adapter/ListDropDownAdapter.kt
@@ -0,0 +1,70 @@
+package com.shuyu.github.kotlin.ui.adapter
+
+/**
+ * Created by guoshuyu
+ * Date: 2018-10-22
+ */
+
+import android.content.Context
+import android.support.v4.content.ContextCompat
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.TextView
+import com.shuyu.github.kotlin.R
+import kotlinx.android.synthetic.main.layout_item_default_drop_down.view.*
+
+
+class ListDropDownAdapter(private val context: Context, private val list: List<String>) : BaseAdapter() {
+    private var checkItemPosition = 0
+
+    fun setCheckItem(position: Int) {
+        checkItemPosition = position
+        notifyDataSetChanged()
+    }
+
+    override fun getCount(): Int {
+        return list.size
+    }
+
+    override fun getItem(position: Int): Any? {
+        return null
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getView(position: Int, convert: View?, parent: ViewGroup): View {
+        var convertView = convert
+        var viewHolder: ViewHolder?
+        if (convertView != null) {
+            viewHolder = convertView.tag as ViewHolder
+        } else {
+            convertView = LayoutInflater.from(context).inflate(R.layout.layout_item_default_drop_down, null)
+            viewHolder = ViewHolder()
+            viewHolder.text = convertView.item_default_drop_text
+            convertView.tag = viewHolder
+        }
+        fillValue(position, viewHolder)
+        return convertView!!
+    }
+
+    private fun fillValue(position: Int, viewHolder: ViewHolder?) {
+        viewHolder?.text?.text = list[position]
+        if (checkItemPosition != -1) {
+            if (checkItemPosition == position) {
+                viewHolder?.text?.setTextColor(ContextCompat.getColor(context, R.color.colorPrimary))
+                viewHolder?.text?.setBackgroundResource(R.color.check_bg)
+            } else {
+                viewHolder?.text?.setTextColor(ContextCompat.getColor(context, R.color.subTextColor))
+                viewHolder?.text?.setBackgroundResource(R.color.white)
+            }
+        }
+    }
+
+    internal class ViewHolder {
+        var text: TextView? = null
+    }
+}
