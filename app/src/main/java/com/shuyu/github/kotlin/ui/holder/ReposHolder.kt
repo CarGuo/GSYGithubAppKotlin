d341aaf5c1b240c11423948f64e0b04a01d260c3
Shuyu Guo <359369982@qq.com>
2018-10-24 21:09:29 +0800

    go to person

--- app/src/main/java/com/shuyu/github/kotlin/ui/holder/ReposHolder.kt
+++ app/src/main/java/com/shuyu/github/kotlin/ui/holder/ReposHolder.kt
@@ -6,17 +6,22 @@ import android.view.View
 import com.shuyu.github.kotlin.R
 import com.shuyu.github.kotlin.databinding.LayoutReposItemBinding
 import com.shuyu.github.kotlin.model.ui.ReposUIModel
+import com.shuyu.github.kotlin.module.person.PersonActivity
 import com.shuyu.github.kotlin.ui.holder.base.DataBindingHolder
+import kotlinx.android.synthetic.main.layout_repos_item.view.*
 
 
 /**
  * 事件item
  */
-class ReposHolder(context: Context, v: View, dataBing: ViewDataBinding) : DataBindingHolder<ReposUIModel, LayoutReposItemBinding>(context, v, dataBing) {
+class ReposHolder(context: Context, private val v: View, dataBing: ViewDataBinding) : DataBindingHolder<ReposUIModel, LayoutReposItemBinding>(context, v, dataBing) {
 
 
     override fun onBind(model: ReposUIModel, position: Int, dataBing: LayoutReposItemBinding) {
         dataBing.reposUIModel = model
+        v.repos_user_img.setOnClickListener {
+            PersonActivity.gotoPersonInfo(model.ownerName)
+        }
     }
 
     companion object {
