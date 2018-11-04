8b8bd1d6bb1d62006d42a18a70d7e625ef890a6f
Shuyu Guo <359369982@qq.com>
2018-11-04 20:34:34 +0800

    search drawer

--- app/src/main/java/com/shuyu/github/kotlin/module/search/SearchActivity.kt
+++ app/src/main/java/com/shuyu/github/kotlin/module/search/SearchActivity.kt
@@ -1,9 +1,18 @@
 package com.shuyu.github.kotlin.module.search
 
+import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v4.view.GravityCompat
+import android.view.Menu
+import android.view.MenuItem
 import com.alibaba.android.arouter.facade.Postcard
 import com.alibaba.android.arouter.facade.annotation.Route
 import com.alibaba.android.arouter.launcher.ARouter
+import com.mikepenz.materialdrawer.Drawer
+import com.mikepenz.materialdrawer.DrawerBuilder
+import com.mikepenz.materialdrawer.model.DividerDrawerItem
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem
 import com.shuyu.github.kotlin.R
 import com.shuyu.github.kotlin.module.ARouterAddress
 import com.shuyu.github.kotlin.module.base.BaseFragmentActivity
@@ -25,6 +34,43 @@ class SearchActivity : BaseFragmentActivity() {
                     .build(uri)
         }
     }
+
+
+    private var drawer: Drawer? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val item1 = PrimaryDrawerItem().withIdentifier(1).withName(R.string.app_name)
+        val item2 = SecondaryDrawerItem().withIdentifier(2).withName(R.string.app_name)
+
+        drawer = DrawerBuilder()
+                .withActivity(this)
+                .withDrawerGravity(GravityCompat.END)
+                .withMultiSelect(true)
+                .addDrawerItems(
+                        item1,
+                        DividerDrawerItem(),
+                        item2,
+                        SecondaryDrawerItem().withName(R.string.app_name)
+                ).build()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu_filter, menu)
+        return true
+    }
+
+
+    override fun onMenuItemClick(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            R.id.action_filter -> {
+                drawer?.openDrawer()
+            }
+        }
+        return true
+    }
+
 
     override fun getToolBarTitle(): String = getString(R.string.search)
     override fun getInitFragment(): Fragment = SearchFragment()
